2d7ad2145ae87afafaa4224dcbf0f8c5473a6fbf
sandijean90 <sandibesen@gmail.com>
2025-05-28 11:35:33 -0700

    feat: Added helper functions for flattening messages (from acp input to framework input) and packaging response for acp comatability (from framework output to acp output).
    refactor:  TicketResponse Agent to use helper functions for acp compatability

--- TicketResponseAgent/agent.py
+++ TicketResponseAgent/agent.py
@@ -8,6 +8,8 @@ from pydantic_ai.providers.openai import OpenAIProvider
 from acp_sdk.models import Message, MessagePart
 from acp_sdk.server import RunYield, RunYieldResume, Server
 from collections.abc import AsyncGenerator
+#Helpers
+from helpers import package_response, flatten_messages
 
 #load environment variables
 from dotenv import load_dotenv
@@ -17,20 +19,35 @@ load_dotenv()
 server = Server()
 
 
+# @server.agent()
+# async def ticket_response_agent(input: list[Message]) -> AsyncGenerator[RunYield, RunYieldResume]:
+#     """
+#     An agent that responds to customer support tickets with a simple sentence.
+#     """
+#     user_prompt = str(input[0]).strip()
+
+#     model = OpenAIModel('gpt-4.1-mini-2025-04-14', provider=OpenAIProvider(api_key=os.getenv('OPENAI_API_KEY')))
+#     TicketResponseAgent = Agent( model=model,
+#                             system_prompt=("You are a helpful assistant that responds to customer support tickets in one simple sentence."))
+#     response = await TicketResponseAgent.run(user_prompt)
+    
+#     assistant_message = Message(parts=[MessagePart(content=response.output)])
+#     yield {"messages": [assistant_message]}
+
 @server.agent()
 async def ticket_response_agent(input: list[Message]) -> AsyncGenerator[RunYield, RunYieldResume]:
     """
     An agent that responds to customer support tickets with a simple sentence.
     """
-    user_prompt = str(input[0]).strip()
+    user_prompt = flatten_messages(input)
 
     model = OpenAIModel('gpt-4.1-mini-2025-04-14', provider=OpenAIProvider(api_key=os.getenv('OPENAI_API_KEY')))
     TicketResponseAgent = Agent( model=model,
                             system_prompt=("You are a helpful assistant that responds to customer support tickets in one simple sentence."))
-    response = await TicketResponseAgent.arun(user_prompt)
+    response = await TicketResponseAgent.run(user_prompt)
     
-    assistant_message = Message(parts=[MessagePart(content=response.output)])
-    yield {"messages": [assistant_message]}
+    yield package_response(response.output)
+
 
 #Run the server
 server.run()
