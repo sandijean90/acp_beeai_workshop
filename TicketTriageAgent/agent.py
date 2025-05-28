b3675ac257b86a3684ae3819aff71bcf99704088
sandijean90 <sandibesen@gmail.com>
2025-05-28 12:15:57 -0700

    refactor: changed the TicketTriageAgent to use the helper functions and be ACP compatible.

--- TicketTriageAgent/agent.py
+++ TicketTriageAgent/agent.py
@@ -10,31 +10,39 @@ from beeai_framework.backend import UserMessage, SystemMessage
 from beeai_framework.adapters.acp.serve._utils import acp_msg_to_framework_msg
 from beeai_framework.adapters.acp.serve.server import ACPServer, ACPServerConfig, to_acp_agent_metadata
 from beeai_framework.adapters.acp.serve.agent import ACPServerAgent
-from beeai_framework.agents.react import ReActAgent
+#ACP SDK
+from acp_sdk.models import Message, MessagePart
+from acp_sdk.server import RunYield, RunYieldResume, Server
+from collections.abc import AsyncGenerator
+#Helpers
+from helpers import package_response, flatten_messages
 
+#load environment variables
+from dotenv import load_dotenv
+load_dotenv()
 
+# Set up the ACP server
+server = Server()
 
 
 #Create Agent using the Framework
 class TicketClassifierOutput(BaseModel):
         answer: str = Field(description="your final answer")
 
-async def TicketClassifierAgent() -> None:
-    llm = OpenAIChatModel("gpt-4.1-mini-2025-04-14")
-    system_message = SystemMessage("You are a helpful geography expert. Always provide accurate and concise answers.")
-    user_message = UserMessage("How many islands make up the country of Cape Verde?")
-    response = await llm.create_structure(
-        schema=TicketClassifierOutput,
-        messages=[system_message,user_message],
-    )
-    print(response.object)
+@server.agent()
+async def ticket_triage_agent(input: list[Message]) -> AsyncGenerator[RunYield, RunYieldResume]:
+        """ An agent that classifies customer support tickets.
+        """
+        user_prompt = flatten_messages(input[-1:])
+        llm = OpenAIChatModel("gpt-4.1-mini-2025-04-14")
+        system_msg = SystemMessage(
+            "You are a helpful geography expert. Always provide accurate and concise answers."
+        )
+        response = await llm.create_structure(
+            schema=TicketClassifierOutput,
+            messages=[system_msg, UserMessage(user_prompt)],
+        )
+        yield package_response(response.object.get("answer", ""))
 
-
-# Create and start the server
-server = ACPServer(config=ACPServerConfig(host="0.0.0.0", port=8000))
-server.register(ACPServerAgent(
-      fn=TicketClassifierAgent,
-      name="ticket_classifier",
-      description="Answers geography questions about Cape Verde"))
-
-server.serve()
+#Run the server
+server.run()
